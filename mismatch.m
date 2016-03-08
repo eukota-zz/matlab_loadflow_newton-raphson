47e176a020861f7c8a62a2fb3625af1739fea4c4
Darrell Ross <eukota@gmail.com>
2016-03-08 01:11:13 -0600

    Updated mismatch to only use BusTypes instead of the entire busdata just to get BusTypes.

--- mismatch.m
+++ mismatch.m
@@ -7,25 +7,25 @@
 % * *Q*: vector of reactive power values
 % * *V*: vector of voltage values
 % * *T*: vector of theta values
-% * *busdata*: bus data (need to be but type vector)
+% * *BusTypes*: bus type vector (1=slack,2=pq,3=pv)
 % * *ybus*: full ybus matrix
 %%% OUTPUTS
 % * *Pmm*: mismatch for P
 % * *Qmm*: mismatch for Q
-function [Pmm,Qmm]=mismatch(P,Q,V,T,busdata,ybus)
-    [pcount,qcount]=jacobianCount(busdata);
+function [Pmm,Qmm,err]=mismatch(P,Q,V,T,BusTypes,ybus)
+    [pcount,qcount,err]=jacobianCount(BusTypes);
     mismatch_index_P=1;
     mismatch_index_Q=1;
     Pmm=zeros(pcount,1);
     Qmm=zeros(qcount,1);
-    [buscount,]=size(busdata);
+    buscount=length(BusTypes);
     for n=1:buscount
-        if(busdata(n,2)==1) % Slack
+        if(BusTypes(n)==1) % Slack
             continue;
         end
         Pmm(mismatch_index_P,1)=pfunc(n,V,T,ybus)-P(n);
         mismatch_index_P=mismatch_index_P+1;
-        if busdata(n,2)==2 % PQ 
+        if BusTypes(n)==2 % PQ 
             Qmm(mismatch_index_Q,1)=qfunc(n,V,T,ybus)-Q(n);
             mismatch_index_Q=mismatch_index_Q+1;
         end
