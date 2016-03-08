9a60300f5ca5d44b760aa0c343dc57fb78803b4e
Darrell Ross <eukota@gmail.com>
2016-03-08 01:11:32 -0600

    basic naming changes to master.

--- master.m
+++ master.m
@@ -2,18 +2,18 @@
 % Runs all problems
 
 clear all;
-clc;
+%clc;
 
 %% Test System A
 % 4-bus
 fprintf('-------------------------------------------------------\n');
 disp('Test System A: 4 Bus');
 [abus,abranch]=tsa(); % load data
-nrpf(abus,abranch,1);
+amap=nrpf(abus,abranch,0);
 
 %% Test System B
 % 14-bus
 fprintf('-------------------------------------------------------\n');
 disp('Test System B: 14 Bus');
 [bbus,bbranch]=tsb;
-nrpf(bbus,bbranch,1);
+bmap=nrpf(bbus,bbranch,0);
