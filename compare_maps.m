62588fb2a9e88e493a5620b54b149de2af2ffbd0
Darrell Ross <eukota@gmail.com>
2016-03-10 13:14:32 -0600

    Updated compare_maps to note differences in keys since changing keys doesn't make the values fail.

--- compare_maps.m
+++ compare_maps.m
@@ -14,16 +14,14 @@ function [err]=compare_maps(mapL,mapR)
     keysL = mapL.keys;
     keysR = mapR.keys;
 
-    % If the keys are not equal there is no reason to loop
-    if ~isequal(keysL, keysR)
-        err='Maps are not equal';
-        return;
-    end
-
     % Since at this point it is known that keysL == keysR, keysL is used as a base
     results = zeros(size(keysL));
     for i = 1:length(keysL)
         value=mapL(keysL{i});
+        if(mapR.isKey(keysL{i})==0)
+            fprintf('Key "%s" in mapL is missing in mapR\n',keysL{i});
+            continue;
+        end
         if(isa(value,'containers.Map'))
             itermapL=mapL(keysL{i});
             itermapR=mapR(keysL{i});
@@ -33,7 +31,16 @@ function [err]=compare_maps(mapL,mapR)
                 results(i)=results(i) && all(all(itermapL(iterKeys{j}) == itermapR(iterKeys{j})));
             end
         else
-            results(i) = all(all(mapL(keysL{i}) == mapR(keysL{i})));
+            results(i) = all(size(mapL(keysL{i})) == size(mapR(keysL{i})));
+            if(results(i)==1)
+                results(i) = all(all(mapL(keysL{i}) == mapR(keysL{i})));
+            end
+        end
+    end
+    for i = 1:length(keysR)
+        if(mapL.isKey(keysR{i})==0)
+            fprintf('Key "%s" in mapR is missing in mapL\n',keysL{i});
+            continue;
         end
     end
 
