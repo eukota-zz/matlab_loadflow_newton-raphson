53b1f5fb6da88a601096666d221854eb12f09643
Darrell Ross <eukota@gmail.com>
2016-03-10 19:39:25 -0600

    Bells and whistles added to latexprint.

--- latexprint.m
+++ latexprint.m
@@ -1,13 +1,38 @@
 %% mattex
 % prints matrix ready for LaTeX tabular environment
-function latexprint(mat)
+%% INPUTS
+% * *mat*: matrix to print
+% * *style*: defaults to matrix with & and //, 1 eliminates &, 2 eliminates & and //
+function latexprint(mat,style)
+    if(nargin<2)
+        style=0;
+    end
     [r,c]=size(mat);
+
     for(i=1:r)
-        fprintf('%f',mat(i,1));
-        for(j=2:c)
-            fprintf('   &   ');
-            fprintf('%f',mat(i,j));
+        num=nearzero(mat(i,1));
+        if(num==0)
+            fprintf('%d',num);
+        else
+            fprintf('%f',num);
         end
-        fprintf('\\\\\n');
+        for(j=2:c)
+            if(style==1 || style==2)
+                fprintf(' ');
+            else
+                fprintf('   &   ');
+            end
+            num=nearzero(mat(i,j));
+            if(num==0)
+                fprintf('%d',num);
+            else
+                fprintf('%f',num);
+            end
+        end
+        if(style==2)
+            fprintf('\n');
+        else
+            fprintf('\\\\\n');
+        end
     end
 end
